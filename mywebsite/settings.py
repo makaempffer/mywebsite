14d6a253e1be1cd50bc5cb2428ea2310d5d6f0be
lalasoja <la.ancapi@duocuc.cl>
2022-06-13 19:06:37 -0400

    static corregido

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -116,8 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATICFILES_DIRS = [
+   BASE_DIR / 'mywebsite/static/'
+]
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
