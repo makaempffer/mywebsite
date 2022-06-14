02d31bb8ff96eb3e5c06bb691c6ed7de7356b728
lalasoja <la.ancapi@duocuc.cl>
2022-06-14 00:58:42 -0400

    carpeta static reubicada

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR  / 'static'
+    BASE_DIR / 'mywebsite' / 'static'
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/ 'mywebsite' / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
