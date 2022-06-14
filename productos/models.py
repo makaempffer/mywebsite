3845911519f66dad4abb3fe7e682e083b4bf194b
lalasoja <la.ancapi@duocuc.cl>
2022-06-14 01:06:24 -0400

    carpeta MEDIA

--- productos/models.py
+++ productos/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.conf import settings
+
 class Mascota(models.Model):
     nombre = models.TextField(max_length=64)
     descripcion = models.TextField(max_length=512, default='')
@@ -11,7 +13,7 @@ class Categoria(models.Model):
     nombre = models.TextField(max_length=64)
     descripcion = models.TextField(max_length=512, default='')
     mascota = models.ForeignKey(Mascota, related_name='mascota', on_delete=models.CASCADE, null=True)
-    imagen = models.ImageField(upload_to='static/categorias/', null=True)
+    imagen = models.ImageField(upload_to=settings.MEDIA_ROOT / 'categorias', null=True)
 
     def __str__(self) -> str:
         return f'{self.nombre} ({self.mascota.nombre})'
@@ -23,7 +25,7 @@ class Producto(models.Model):
     descripcion = models.TextField(max_length=512, blank=False)
     precio = models.PositiveIntegerField(blank=False)
     stock = models.IntegerField(blank=False)
-    imagen = models.ImageField(upload_to='static/productos/', null=True)
+    imagen = models.ImageField(upload_to=settings.MEDIA_ROOT / 'productos', null=True)
 
     def __str__(self) -> str:
         return f'{self.nombre} - {str(self.categoria)}'
